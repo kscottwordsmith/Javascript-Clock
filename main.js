4b2d310ea33aa32383b4934919a612453be5cace
kscottwordsmith <kscottwordsmith@gmail.com>
2018-12-17 12:44:24 -0800

    clock done

---
+++ main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+	setInterval(function(){
+		var time = new Date()
+		var clockPut = document.getElementById('clocktime')
+		clockPut.innerHTML = `${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}`
+	}, 1000)
+})
+
+//toLocaleTimeString('en-US')
